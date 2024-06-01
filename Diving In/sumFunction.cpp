7eb3a8b026473a54e52281e1d30a8169ca570229
mmitri <88552429+mmitri@users.noreply.github.com>
2024-06-01 10:52:44 -0400

    challenge code from video that multiplies two numbers

--- Diving In/sumFunction.cpp
+++ Diving In/sumFunction.cpp
@@ -6,6 +6,12 @@ int sumFunction(int first_num, int second_num){
     return sum;
 }
 
+// challenge from video
+int multiplyFunction(int first_num, int second_num){
+    int sum = first_num * second_num;
+    return sum;
+}
+
 int main(){
     int first_num = 10;
     int second_num = 34;
@@ -14,5 +20,6 @@ int main(){
     cout << "Second Num: " << second_num << endl;
     cout << "1. Sum of two numbers is: " << sum << endl;
     cout << "2. Sum of two numbers is: " << sumFunction(99,23) << endl;
+    cout << "3. Product of two numbers is: " << multiplyFunction(first_num,second_num) << endl;
     return 0;
 }
