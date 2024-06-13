6ccb57e9f7c08cc7db575dde4fe4898a431bbcb0
mmitri <88552429+mmitri@users.noreply.github.com>
2024-06-13 09:56:15 -0400

    basic operation example

---
+++ Operations on Data/basicOps.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num1 = 45;
+    int num2 = 56;
+    int num3 = -2;
+    double num4 = 1.23;
+    int num5 = 31;
+    int num6 = 10;
+
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    cout << num2 << " - " << num3 << " = " << num2 - num3 << endl;
+    cout << num4 << " * " << num5 << " = " << num4 * num5 << endl;
+    cout << num5 << " / " << num6 << " = " << num5 / num6 << endl;
+    cout << num5 << " % " << num6 << " = " << num5 % num6 << endl;
+
+    return 0;
+}
