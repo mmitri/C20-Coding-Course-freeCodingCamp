4dc56818191c051c53198e60f1edf34ba2e26ccc
mmitri <88552429+mmitri@users.noreply.github.com>
2026-04-10 13:34:59 -0400

    Reorder output statements in autoKeyword.cpp for clarity

--- Variables and Data Types/autoKeyword.cpp
+++ Variables and Data Types/autoKeyword.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(){
+    // auto to deduce the type of the variable
     auto var1 = 12;
     auto var2 = 13.0;
     auto var3 = 14.0f;
@@ -12,14 +13,14 @@ int main(){
     auto var7 = 123ul;
     auto var8 = 123ll;
 
-    cout << "sizeof(" << var2 << ")  int:           " << sizeof(var2) << " bytes" << endl;
-    cout << "sizeof(" << var3 << ")  double:        " << sizeof(var3) << " bytes" << endl;
-    cout << "sizeof(" << var4 << ")  float:         " << sizeof(var4) << " bytes" << endl;
-    cout << "sizeof(" << var5 << ")   long double:   " << sizeof(var5) << " bytes" << endl;
-    cout << "sizeof(" << var6 << ") char:          " << sizeof(var6) << " bytes" << endl;
-    cout << "sizeof(" << var7 << ") unsigned int:  " << sizeof(var7) << " bytes" << endl;
-    cout << "sizeof(" << var8 << ") unsigned long: " << sizeof(var8) << " bytes" << endl;
-    cout << "sizeof(" << var1 << ")  long long:     " << sizeof(var1) << " bytes" << endl;
+    cout << "sizeof(" << var1 << ")  int:           " << sizeof(var1) << " bytes" << endl;
+    cout << "sizeof(" << var2 << ")  double:        " << sizeof(var2) << " bytes" << endl;
+    cout << "sizeof(" << var3 << ")  float:         " << sizeof(var3) << " bytes" << endl;
+    cout << "sizeof(" << var4 << ")   long double:   " << sizeof(var4) << " bytes" << endl;
+    cout << "sizeof(" << var5 << ") char:          " << sizeof(var5) << " bytes" << endl;
+    cout << "sizeof(" << var6 << ") unsigned int:  " << sizeof(var6) << " bytes" << endl;
+    cout << "sizeof(" << var7 << ") unsigned long: " << sizeof(var7) << " bytes" << endl;
+    cout << "sizeof(" << var8 << ") long long:     " << sizeof(var8) << " bytes" << endl;
 
     return 0;
 }
